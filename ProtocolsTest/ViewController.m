f84cbeb8227be4912d254b88ce531035eff1c541
Gosc 01 <gosc01@iMac-15.local>
2013-11-05 12:40:12 +0100

    Add creating data source of pie chart and the actual Char Pie

--- ProtocolsTest/ViewController.m
+++ ProtocolsTest/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "MyDataSource.h"
+#import "PieChart.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSUInteger numberOfPieces = 5;
+    
+    NSArray* arrayOfSizes = @[[NSNumber numberWithFloat:9.3], [NSNumber numberWithFloat:6.3], [NSNumber numberWithFloat:10.3] , [NSNumber numberWithFloat:3.3], [NSNumber numberWithFloat:7.3] ];
+    
+    NSArray * arrayOfTitles = @[@"Roak",@"Cechy", @"Sjuup", @"Pociechac", @"Magic"];
+    
+    MyDataSource * mds = [[MyDataSource alloc] initWithNumberOfSegments:numberOfPieces andSizeArray:arrayOfSizes andTitleArray:arrayOfTitles];
+    
+    PieChart * pc = [[PieChart alloc] initWithDataSource:mds];
+    
+    
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
